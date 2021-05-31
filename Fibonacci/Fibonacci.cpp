0b3b56e6f83a244c15aa503cff7df76b8dded4ee
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2021-05-31 19:32:13 +0200

    Update Fibonacci.cpp
    +Adds Fibonacci recursive function

--- Fibonacci/Fibonacci.cpp
+++ Fibonacci/Fibonacci.cpp
@@ -1,5 +1,18 @@
+#include <iostream>
+
+int FibonacciRecursive(int);
+
 
 int main(){
     
+    const int number = 7;
+    std::cout << FibonacciRecursive(number) << std::endl;
+    
     return 0;
 }
+int FibonacciRecursive(const int n){
+    if(n <= 1)
+        return n;
+    return FibonacciRecursive(n-1)+FibonacciRecursive(n-2);
+}
+
