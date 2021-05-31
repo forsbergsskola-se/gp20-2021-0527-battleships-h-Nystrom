82563f04fa6fe1c4b72f35e418f973561922cc7b
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2021-05-31 20:54:00 +0200

    Update Fibonacci.cpp
    Adds iterative Fibonacci function

--- Fibonacci/Fibonacci.cpp
+++ Fibonacci/Fibonacci.cpp
@@ -1,13 +1,20 @@
+using namespace std;
 #include <iostream>
 
+
 int FibonacciRecursive(int);
+int FibonacciIterative(int);
 
 
 int main(){
     
-    const int number = 7;
-    std::cout << FibonacciRecursive(number) << std::endl;
-    
+    const int length = 10;
+    for(int n = 0; n <= length; n++){
+        cout << FibonacciIterative(n) << endl;
+    }
+    for (int n = 0; n <= length; n++){
+        cout << FibonacciRecursive(n) << endl;
+    }
     return 0;
 }
 int FibonacciRecursive(const int n){
@@ -15,4 +22,18 @@ int FibonacciRecursive(const int n){
         return n;
     return FibonacciRecursive(n-1)+FibonacciRecursive(n-2);
 }
+
+int FibonacciIterative(const int n){
+    if(n <= 1)
+        return n;
+    int previousNumber = 0;
+    int previousNumber2 = 1;
+    for (int i = 1; i < n; i++){
+        const int next = previousNumber + previousNumber2;
+        previousNumber = previousNumber2;
+        previousNumber2 = next;
+    }
+    return previousNumber2;
+}
+
 
