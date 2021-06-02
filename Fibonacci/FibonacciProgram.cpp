dca5eb2ff6cccc9d85ff6a933e100a589faacbcc
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2021-06-02 09:59:29 +0200

    Update FibonacciProgram.cpp
    To see performance differences better.

--- Fibonacci/FibonacciProgram.cpp
+++ Fibonacci/FibonacciProgram.cpp
@@ -3,7 +3,7 @@
 int main(){
     auto fibonacciIterative = FibonacciIterative();
     auto fibonacciRecursive = FibonacciRecursive();
-    fibonacciIterative.DisplayElapsedTimeFor(10);
-    fibonacciRecursive.DisplayElapsedTimeFor(10);
+    fibonacciIterative.DisplayElapsedTimeFor(30);
+    fibonacciRecursive.DisplayElapsedTimeFor(30);
     return 0;
 }
