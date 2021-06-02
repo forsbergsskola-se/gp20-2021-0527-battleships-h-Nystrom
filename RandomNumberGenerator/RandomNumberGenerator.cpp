84d0899a31fec9dc8f5361601ab2a0fb746f6c25
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2021-06-02 12:12:52 +0200

    Update RandomNumberGenerator.cpp
    +Adds random seeds from current time
    +Fixes max value issue

--- RandomNumberGenerator/RandomNumberGenerator.cpp
+++ RandomNumberGenerator/RandomNumberGenerator.cpp
@@ -1,28 +1,28 @@
-#include <algorithm>
 #include <iostream>
+#include <time.h>
 
 int main(){
-    const int maxValue = 20;
-    const int minValue = 1;
-    const int maxLength = 20;
-    int randomNumbers[maxLength];
-    int numberFrequency[maxLength];
-    std::fill_n(numberFrequency, maxLength,0);
+    const int maxLength = 21;
+    int randomNumbers[maxLength]{};
+    int numberFrequency[maxLength]{};
+    srand(static_cast<unsigned>(time(nullptr)));
     
     for (int i = 0; i < maxLength;i++){
+        const int minValue = 1;
+        const int maxValue = 20;
         const int tempRandom =  rand() % maxValue+minValue;
         randomNumbers[i] = tempRandom;
         numberFrequency[tempRandom]++;
     }
     for (int i = 0; i < maxLength;i++){
-       std::cout << randomNumbers[i] << " ";
+       std::cout << randomNumbers[i] << " " << std::flush;
     }
-    std::cout << " " << std::endl;
+    std::cout << std::endl;
     for (int i = 0; i < maxLength; i++){
     if(numberFrequency[i] <= 0)
         continue;
-    const float percentageChance = static_cast<float>(numberFrequency[i])/static_cast<float>(maxLength)*100;
-    std::cout << "Frequency "<< i << ": " << numberFrequency[i] << "times " << percentageChance << "% " << std::endl;    
+    const float percentageChance = static_cast<float>(numberFrequency[i])/static_cast<float>(maxLength-1)*100;
+    std::cout << "Frequency "<< i << ": " << numberFrequency[i] << "times " << percentageChance << "% " << std::endl;
     }
     return 0;
 }
