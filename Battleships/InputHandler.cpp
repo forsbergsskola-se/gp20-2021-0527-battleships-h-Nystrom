85b84a89b35e261b02f1d3e6577e51153024cd37
h-Nystrom <71410719+h-Nystrom@users.noreply.github.com>
2021-06-04 14:23:31 +0200

    Refactoring adds generic method for input handling

--- Battleships/InputHandler.cpp
+++ Battleships/InputHandler.cpp
@@ -1,15 +1,19 @@
 ﻿#include "stdafx.h"
 #include "InputHandler.h"
 
+template<typename TValue>
+TValue InputHandler::GetInputValue(){
+    TValue targetValue;
+    cin >> noskipws >> targetValue;
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(),'\n');
+    return targetValue;
+}
 
-//TODO: If I have time refactor duplication...
 void InputHandler::EndCurrentTurn(int playerId){
     for(int i = 0; i < 2; i++){
-        int playerInput;
         cout << "Player" << playerId << " press enter!" << endl;
-        cin >> noskipws >> playerInput;
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(),'\n');
+        GetInputValue<int>();
         playerId++;
     }
     system("CLS");
@@ -17,27 +21,20 @@ void InputHandler::EndCurrentTurn(int playerId){
 
 bool InputHandler::SimpleRequest(const char trueChar, const char falseChar, const string message){
     while (true){
-        char playerInput;
         cout << message << endl;
-        cin >> noskipws >> playerInput;
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(),'\n');
+        const char playerInput = GetInputValue<char>();
         if(playerInput == trueChar)
             return true;
         if(playerInput == falseChar)
             return false;
-        
         cout << "Input needs to be: " << trueChar << ", or: " << falseChar << endl;
     }
 }
 
 int InputHandler::CoordinateToIndex(const int minValue, const int maxValue, const string message){
     while (true){
-        string playerInput;
         cout << message << endl;
-        cin >> noskipws >> playerInput;
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(),'\n');
+        const auto playerInput = GetInputValue<string>();
         const int gridIndex = StringToInt(playerInput, minValue,maxValue);
         if(gridIndex != -1)
             return gridIndex;
--- Battleships/InputHandler.h
+++ Battleships/InputHandler.h
@@ -4,6 +4,8 @@
 class InputHandler{
     static int StringToInt(string coordinate, int minValue,int maxValue);
 public:
+    template<typename TValue>
+    static TValue GetInputValue();
     static bool SimpleRequest(char trueChar, char falseChar, string message);
     static int CoordinateToIndex(int minValue,int maxValue, string message);
     static void EndCurrentTurn(int playerId);
